76484161f2220fceb4c7ead312f656fd15d58055
Gustavo Andres Santoyo Benavides <150487562+DocGus@users.noreply.github.com>
2025-04-18 03:11:54 +0000

    tercera entrega animo

--- test.js
+++ test.js
@@ -3,33 +3,51 @@ const { sum } = require('./app.js');
 
 
 test('adds 14 + 9 to equal 23', () => {
-  
+
     let total = sum(14, 9);
 
 
     expect(total).toBe(23);
 });
 
-test("One euro should be 1.206 dollars", function(){
+test("One euro should be 1.07 dollars", function () {
 
-    const { fromEuroToDollar } = require('./app.js')
-    
+    const { fromEuroToDollar } = require('./app.js');
 
-    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+
+    const dollars = fromEuroToDollar(3.5);
+
+
+    const expected = 3.5 * 1.07;
+
+
+    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 
-test("One euro should be 1.206 dollars", function(){
- 
-    const { fromDollarToYen } = require('./app.js')
-    
+test("One euro should be 156.5 yens", function () {
 
-    expect(fromDollarToYen(1)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+    const { fromEuroToYen } = require('./app.js');
+
+
+    const yens = fromEuroToYen(3.5);
+
+
+    const expected = 3.5 * 156.5;
+
+    expect(fromEuroToYen(3.5)).toBe(547.75); // 1 euro son 156.5 yens, entonces 3.5 euros deberían ser = (3.5 * 156.5)
 })
 
-test("One euro should be 1.206 dollars", function(){
 
-    const { fromYenToPound } = require('./app.js')
-    
+test("One euro should be 0.87 pounds", function () {
 
-    expect(fromYenToPound(1)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+    const { fromEuroToPound } = require('./app.js');
+
+
+    const yens = fromEuroToPound(3.5);
+
+
+    const expected = 3.5 * 0.87;
+
+
+    expect(fromEuroToPound(3.5)).toBe(3.045); // 1 euro son 0.87 pounds, entonces 3.5 euros deberían ser = (3.5 * 0.87)
 })
